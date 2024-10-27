69e127a1e63f068503bf049499af70d971d8a117
yuri-italo <yuri.italo94@gmail.com>
2024-10-27 14:12:49 -0300

    feat: initial structure

---
+++ src/test/java/dev/yuri/addresses_api/AddressesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yuri.addresses_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
