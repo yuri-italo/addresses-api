9b7716a011210ae12c1a89370b1719a73b4137ab
yuri-italo <yuri.italo94@gmail.com>
2024-11-12 22:27:59 -0300

    fix: add better error's responses

--- src/main/java/dev/yuri/addresses_api/dto/response/ErrorResponse.java
+++ src/main/java/dev/yuri/addresses_api/dto/response/ErrorResponse.java
@@ -1,4 +1,9 @@
 package dev.yuri.addresses_api.dto.response;
 
-public record ErrorResponse(String mensagem, int status) {
+import java.time.LocalDateTime;
+
+public record ErrorResponse(int status, String error, String mensagem, LocalDateTime timestamp, String path) {
+    public ErrorResponse(int status, String error, String mensagem, String path) {
+        this(status, error, mensagem, LocalDateTime.now(), path);
+    }
 }
--- src/main/java/dev/yuri/addresses_api/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/yuri/addresses_api/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package dev.yuri.addresses_api.exception;
 
 import dev.yuri.addresses_api.dto.response.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -8,59 +9,88 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 
-import java.util.HashMap;
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, Object>> handleValidationExceptions(
-            MethodArgumentNotValidException ex, WebRequest request) {
+            MethodArgumentNotValidException ex, HttpServletRequest request) {
 
-        Map<String, Object> errors = new HashMap<>();
+        Map<String, Object> errors = new LinkedHashMap<>();
         FieldError firstError = ex.getBindingResult().getFieldErrors().stream().findFirst().orElse(null);
 
         if (firstError != null) {
+            errors.put("status", HttpStatus.BAD_REQUEST.value());
+            errors.put("error", HttpStatus.BAD_REQUEST.toString());
             errors.put("mensagem", firstError.getDefaultMessage());
-            errors.put("status", HttpStatus.BAD_REQUEST.value());
+            errors.put("timestamp", LocalDateTime.now());
+            errors.put("path", request.getRequestURI());
         } else {
-            errors.put("mensagem", "Erro de validação desconhecido.");
             errors.put("status", HttpStatus.BAD_REQUEST.value());
+            errors.put("error", HttpStatus.BAD_REQUEST.toString());
+            errors.put("mensagem", "Erro de validação desconhecido.");
+            errors.put("timestamp", LocalDateTime.now());
+            errors.put("path", request.getRequestURI());
         }
 
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<?> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
-        return ResponseEntity.status(400).body(new ErrorResponse(
-        "Erro na leitura dos dados da requisição. Verifique o formato e os valores informados.", 400));
+    public ResponseEntity<?> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpServletRequest request) {
+        return ResponseEntity.status(400)
+            .body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.toString(),
+                "Erro na leitura dos dados da requisição. Verifique o formato e os valores informados.",
+                request.getRequestURI()));
     }
 
     @ExceptionHandler(InvalidFilterException.class)
-    public ResponseEntity<?> handleInvalidFiler(InvalidFilterException ex) {
-        return ResponseEntity.status(400).body(new ErrorResponse(ex.getMessage(), 400));
+    public ResponseEntity<?> handleInvalidFiler(InvalidFilterException ex, HttpServletRequest request) {
+        return ResponseEntity.status(400)
+            .body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.toString(),
+                ex.getMessage(),
+                request.getRequestURI()));
     }
 
     @ExceptionHandler(AddressDoesNotBelongToPersonException.class)
-    public ResponseEntity<?> AddressDoesNotBelongToPersonFiler(AddressDoesNotBelongToPersonException ex) {
-        return ResponseEntity.status(400).body(new ErrorResponse(ex.getMessage(), 400));
+    public ResponseEntity<?> AddressDoesNotBelongToPersonFiler(AddressDoesNotBelongToPersonException ex, HttpServletRequest request) {
+        return ResponseEntity.status(400)
+            .body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.toString(),
+                ex.getMessage(),
+                request.getRequestURI()));
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<?> handleEntityNotFound(EntityNotFoundException ex) {
-       return ResponseEntity.status(404).body(new ErrorResponse(ex.getMessage(), 404));
+    public ResponseEntity<?> handleEntityNotFound(EntityNotFoundException ex, HttpServletRequest request) {
+        return ResponseEntity.status(404)
+            .body(new ErrorResponse(HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.toString(),
+                ex.getMessage(),
+                request.getRequestURI()));
     }
 
     @ExceptionHandler(EntityNotSavedException.class)
-    public ResponseEntity<?> handleEntityNotSaved(EntityNotSavedException ex) {
-        return ResponseEntity.status(500).body(new ErrorResponse(ex.getMessage(), 500));
+    public ResponseEntity<?> handleEntityNotSaved(EntityNotSavedException ex, HttpServletRequest request) {
+        return ResponseEntity.status(500)
+            .body(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.toString(),
+                ex.getMessage(),
+                request.getRequestURI()));
     }
 
     @ExceptionHandler(EntityAlreadyExistsException.class)
-    public ResponseEntity<?> handleEntityAlreadyExists(EntityAlreadyExistsException ex) {
-        return ResponseEntity.status(409).body(new ErrorResponse(ex.getMessage(), 409));
+    public ResponseEntity<?> handleEntityAlreadyExists(EntityAlreadyExistsException ex, HttpServletRequest request) {
+        return ResponseEntity.status(409)
+            .body(new ErrorResponse(HttpStatus.CONFLICT.value(),
+                HttpStatus.CONFLICT.toString(),
+                ex.getMessage(),
+                request.getRequestURI()));
     }
 }
